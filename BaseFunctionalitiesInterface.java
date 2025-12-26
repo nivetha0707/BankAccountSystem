58fff28a8940d0600cd9c1a5f57108313e6246ca
Nivetha S <nivethaaa2007@gmail.com>
2025-12-26 07:44:19 +0530

    Create BaseFunctionalitiesInterface for bank account

---
+++ BaseFunctionalitiesInterface.java
@@ -0,0 +1,18 @@
+package BankAccount;
+
+public interface BaseFunctionalitiesInterface {
+    public int getdepositAmt();
+    public void setdepositAmt(int depamt );
+    public int getwithdrawAmt();
+    public void setwithdrawAmt(int depamt );
+    public int getaccNo();
+    public void setaccNo(int accNo);
+    public int getbalance();
+    public void setbalance(int bal);
+    public String getownerName();
+    public void setownerName(String name);
+    public String getacctype();
+    public void setacctype(String name);
+
+
+}
