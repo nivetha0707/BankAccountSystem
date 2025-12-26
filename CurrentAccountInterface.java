c44506eece82c70b3d18333edbcf4fc09ae46f0f
Nivetha S <nivethaaa2007@gmail.com>
2025-12-26 07:46:57 +0530

    Create CurrentAccountInterface.java

---
+++ CurrentAccountInterface.java
@@ -0,0 +1,6 @@
+package BankAccount;
+
+public interface CurrentAccountInterface extends BaseFunctionalitiesInterface {
+    public String getenableOverdraft();
+    public void setenableOverdraft(String name);
+}
